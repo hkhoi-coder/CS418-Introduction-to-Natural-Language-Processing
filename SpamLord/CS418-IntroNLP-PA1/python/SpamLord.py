49728e12be5642077b526eaaec933ef3d044a203
hkhoi <hkhoi@outlook.com>
2016-02-25 22:34:32 +0700

    updated

--- SpamLord/CS418-IntroNLP-PA1/python/SpamLord.py
+++ SpamLord/CS418-IntroNLP-PA1/python/SpamLord.py
@@ -8,11 +8,10 @@ import pprint
 
 # hkhoi@outlook.com
 email_pattern0 = r'([\w\.]+) ?(@|&#x40;) ?(\w+(\.\w+)+)'        # hkhoi@outlook.com|hkhoi&#x40;outlook.com
-email_pattern1 = r'(\w+) WHERE (\w+) DOM (\w+)'                 # hkhoi WHERE outlook DOM edu
-email_pattern2 = r'\W([a-z]+ at \w+(( dot |\.|;)\w+)+)'         # hkhoi at outlook dot com|hkhoi at outlook.com|hkhoiat outlook;com
+email_pattern1 = r'([\w\.]+) WHERE (\w+) DOM (\w+)'             # hkhoi WHERE outlook DOM edu
+email_pattern2 = r'\W([a-z( dot )]+ at \w+(( dot |\.|;)\w+)+)'         # hkhoi at outlook dot com|hkhoi at outlook.com|hkhoiat outlook;com
 email_pattern3 = r'\(\'([\w\.]+)\',\'([\w\.]+)\'\)'             # ('outlook.com', 'hkhoi')
-email_pattern4 = r'(\w(-\w)+-@-\w(-\w)+-.(-\w)+)'               # w-t-f-i-s-t-h-i-s-s-h-i-t-@-s-h-i-t-.-c-o-m
-email_pattern5 = r'([\w\.]+) ?(@|&#x40;) ?(\w+(\.\w+)+)'
+email_pattern4 = r'(\w(-\w|-\.)+-@-\w(-\w)+-.(-\w)+)'               # w-t-f-i-s-t-h-i-s-s-h-i-t-@-s-h-i-t-.-c-o-m
 
 phone_pattern = r'\D\(?(\d{3})\)?-? ?(\d{3})-(\d{4})'
 
@@ -47,7 +46,6 @@ def process_file(name, f):
         email_matches2 = re.findall(email_pattern2, line)
         email_matches3 = re.findall(email_pattern3, line)
         email_matches4 = re.findall(email_pattern4, line)
-        email_matches5 = re.findall(email_pattern5, line)
 
         # Pattern 0:
         for m in email_matches0:
